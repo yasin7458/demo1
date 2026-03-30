5aac2d408682e3d75b6d64635141bed6face719b
yashin <yashinmithani950@gmail.com>
2026-03-30 16:17:31 +0530

    created dsa file

--- remove.cpp
+++ remove.cpp
@@ -10,7 +10,7 @@ int main()
     {
         for (int j = i + 1; j < n; j++)
         {
-            if (arr[i] ==  arr[j])
+            if (arr[i] == arr[j])
             {
                 for (int k = j; k < n - 1; k++)
                 {
